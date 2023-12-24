e73ad6e8d549905afa10292a287d9a92e23ed175
ihorkoi <koleev.igor@gmail.com>
2023-12-24 14:42:14 +0200

    rewrite on ES6

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+import express from 'express'
+import logger from 'morgan'
+import cors from 'cors'
 
 const contactsRouter = require('./routes/api/contacts')
 
@@ -22,4 +22,4 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message })
 })
 
-module.exports = app
+export default app
