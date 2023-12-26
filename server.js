2126ef781b841a10eb22406c5376deeb9b228585
ihorkoi <koleev.igor@gmail.com>
2023-12-26 12:07:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const { DB_HOST, PORT = 3000 } = process.env;
 mongoose.set("strictQuery", true);
 
 mongoose.connect(DB_HOST)
-  .then(() =>
+  .then(() =>{
     app.listen(PORT, () => {
       console.log("Server running. Use our API on port: 3000");
     })
-  )
+  })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
