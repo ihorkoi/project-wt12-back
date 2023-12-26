fb1776daad82a93a128df6b0518d59f7e9cad437
ihorkoi <koleev.igor@gmail.com>
2023-12-26 13:27:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,17 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
 app.use(cors())
+app.use(function(req, res, next) {
+  const allowedOrigins = ['http://localhost:3000'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
 app.use(express.json())
 
 app.use('/api/auth', authRouter)
